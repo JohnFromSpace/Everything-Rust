8da19d421a513acc7b90dfda50fe729060a72287
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-08 01:07:53 +0200

    Update Data Types.rs

--- Code/Data Types.rs
+++ Code/Data Types.rs
@@ -1,4 +1,11 @@
 fn main() {
+  //Numeric operations 
+    let sum = 5 + 10;
+    let diff = 95.5 - 4.3;
+    let prod = 4 * 3;
+    let div = 55.5 / 11.1;
+    let floor = 2 / 3; // 0
+    let rem = 43 % 5 = // modulo is 3
   
 
 }
