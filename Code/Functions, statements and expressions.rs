f986a63a5f399df5c82ac3dc13fb00a2fa27f2b7
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-08 11:54:47 +0200

    Update Functions, statements and expressions.rs

--- Code/Functions, statements and expressions.rs
+++ Code/Functions, statements and expressions.rs
@@ -1,3 +1,9 @@
 fn main() {
-  
+  print_labeled_measurement(5, 'h');
+}
+
+//does not matter if the function's definition is before or after calling the main function 
+//in other words, there is no need to apply forward declaration as in C/C++ and other languages
+fn print_labeled_measurement(value: i32, unit_label: char) { 
+    println!("The measurement is {value}{unit_label}.");
 }
