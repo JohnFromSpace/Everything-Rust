7864fc04dcc6b5b2e92a60000204fd208654b6e5
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-10 23:40:39 +0200

    Update Ownership.rs

--- Code/Ownership.rs
+++ Code/Ownership.rs
@@ -4,5 +4,15 @@ fn main() {
   {
      let s = "hello"; // valid for now
   } // outside the scope 's' is no longer valid
-
+  
+  let s = String::from("hello"); // this type of "String" Rust has, 
+  // allows us to allocate memory on heap which stores an amount of text 
+  // that is unknown to us at compile time
+  // A string can be created from a string literal by using the function "from"
+  // The double colon "::" allows us to namespace this particular "from" function
+  // under the "String" type rather than using a different name
+  
+  
+  
+    
 }
