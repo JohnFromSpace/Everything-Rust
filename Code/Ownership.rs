520925533504a245d841ec99c48fe83d4e9bc9e2
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-10 23:36:02 +0200

    Update Ownership.rs

--- Code/Ownership.rs
+++ Code/Ownership.rs
@@ -1,1 +1,8 @@
+fn main() {
+  let s = "hello"; // valid until the end of the scope of the main function
+  
+  {
+     let s = "hello"; // valid for now
+  } // outside the scope 's' is no longer valid
 
+}
