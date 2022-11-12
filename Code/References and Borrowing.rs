83fcb6239626a5b67d6f81a7758ea748b02b3332
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-12 23:22:49 +0200

    Update References and Borrowing.rs

--- Code/References and Borrowing.rs
+++ Code/References and Borrowing.rs
@@ -4,9 +4,17 @@ fn main() {
   // it refers to its value without taking ownership of it
   
   println!("The length of '{}' is {}.", s, len);
+  
+  
+  let mut s = String::from("hi");
+  change(&mut s); // we call the function by using a mutable reference 
 }
 
 fn calculate_length(s: &String) -> usize { // 's' is a reference to a "String"
   s.len()
 } // 's' goes out of scope and since it does not have ownership of the it refers to,
 // it is not dropped
+
+fn change(s: &mut String) {
+  s.push_str(", world");
+} 
