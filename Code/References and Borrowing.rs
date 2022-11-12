17d602936afe39d50cf8015e160e9f832ce4cd87
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-12 23:20:42 +0200

    Update References and Borrowing.rs

--- Code/References and Borrowing.rs
+++ Code/References and Borrowing.rs
@@ -1,10 +1,12 @@
 fn main() {
-  let s = String::from("hi");
-  let len = calculate_length(&s);
+  let s = String::from("hi"); 
+  let len = calculate_length(&s); // "&s" creates a reference of the variable 's', i.e.
+  // it refers to its value without taking ownership of it
   
   println!("The length of '{}' is {}.", s, len);
 }
 
-fn calculate_length(s: &String) -> usize {
+fn calculate_length(s: &String) -> usize { // 's' is a reference to a "String"
   s.len()
-}
+} // 's' goes out of scope and since it does not have ownership of the it refers to,
+// it is not dropped
