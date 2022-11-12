a448671a857b6068f23573483668891738b7c76b
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-13 00:15:19 +0200

    Update Slices.rs

--- Code/Slices.rs
+++ Code/Slices.rs
@@ -50,6 +50,12 @@ fn main() {
   // Because string literals *are* string slices already,
   // this works too, without the slice syntax!
   let word = first_word(my_string_literal);
+  
+  
+  // Other slices
+  let a = [1, 2, 3, 4, 5];
+  let slice = &a[1..3]; // &[i32]
+  assert_eq!(slice, &[2..3]); // asserts that two expressions are equal to each other
 }
 
 fn first_word(word: &String) -> usize { 
