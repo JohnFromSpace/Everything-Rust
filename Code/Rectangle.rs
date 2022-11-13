d5274184b62ffe5ed8fde1a31bb76527e1a1dc32
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-14 00:34:22 +0200

    Update Rectangle.rs

--- Code/Rectangle.rs
+++ Code/Rectangle.rs
@@ -1,3 +1,19 @@
 fn main() {
+  let width = 30;
+  let height = 20;
+  println!("The area of the rectangle is {}.", area(width, height)); // 600
+  
+  let rect = (30, 40);
+  println!("The area of the rectangle is {}.", area_tuples(rect));
+  
+  
+  
+}
 
+fn area(w: u32, h: u32) -> u32 {
+  w * h
+}
+
+fn area_tuples(dimensions: (u32, u32)) -> u32 {
+  dimensions.0 * dimensions.1
 }
