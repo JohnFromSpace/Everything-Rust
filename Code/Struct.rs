b43adc00928528354bf9ab604021f9d362f8eb06
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-13 23:43:10 +0200

    Update Struct.rs

--- Code/Struct.rs
+++ Code/Struct.rs
@@ -11,6 +11,8 @@ fn main() {
     username: String::from("someusername123"),
     active: true;
     sign_in_count: 1,
-  }
+  };
+  
+  user1.email = String::from("anotheremail@example.com");
   
 }
