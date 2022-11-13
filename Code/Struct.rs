1e73412d12f5d3d80d7d2da8b00b52f860dae58a
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-13 23:42:21 +0200

    Update Struct.rs

--- Code/Struct.rs
+++ Code/Struct.rs
@@ -7,8 +7,8 @@ struct User {
 
 fn main() {
   let user1 = User {
-    email: String::from("someone@example.com");
-    username: String::from("someusername123");
+    email: String::from("someone@example.com"),
+    username: String::from("someusername123"),
     active: true;
     sign_in_count: 1,
   }
