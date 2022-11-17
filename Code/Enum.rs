bee0044a38ac19f02fdf4da00c4f0a9baba0ba1f
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-17 23:55:13 +0200

    Update Enum.rs

--- Code/Enum.rs
+++ Code/Enum.rs
@@ -1,10 +1,10 @@
 enum IpAddress {
-  v4, 
-  v6,
+  V4, 
+  V6,
 }
 
 fn main() {
   // we can create instances of the "enum" class
-  let v_four = IpAddress::v4;
-  let v_six = IpAddress::v6;
+  let v_four = IpAddress::V4;
+  let v_six = IpAddress::V6;
 }
