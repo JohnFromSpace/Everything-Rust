60ab37ef09ff3404699f79184adeef374df57022
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-17 23:58:54 +0200

    Update Enum.rs

--- Code/Enum.rs
+++ Code/Enum.rs
@@ -1,16 +1,20 @@
-enum IpAddress {
+enum IpAddressVersion {
   V4, 
   V6,
 }
 
+struct IpAddress {
+  version: IpAddressVersion,
+}
+
 // we can call this function with either variant of "IpAddress"
-fn route(ip_address: IpAddress) {}
+fn route(ip_address_version: IpAddressVersion) {}
 
 fn main() {
   // we can create instances of the "enum" class
-  let v_four = IpAddress::V4;
-  let v_six = IpAddress::V6;
+  let v_four = IpAddressVersion::V4;
+  let v_six = IpAddressVersio::V6;
   
-  route(IpAddress::V4);
-  route(v_six); // same as "route(IpAddress::V6);"
+  route(IpAddressVersion::V4);
+  route(v_six); // same as "route(IpAddressVersion::V6);"
 }
