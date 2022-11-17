cb240b5eef9d59ca586d9c42fa861202a72f646c
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-17 23:42:26 +0200

    Update Method Syntax.rs

--- Code/Method Syntax.rs
+++ Code/Method Syntax.rs
@@ -5,8 +5,11 @@ struct Rectangle {
   height: u32,
 }
 
+// function "area" is defined within the context of "Rectangle",
+// thus it will be associated with that type
+// we use the keyword "impl" and after it we add the typename
 impl Rectangle {
-  fn area(&self) -> u32 {
+  fn area(&self) -> u32 { 
     self.width * self.height
   }  
 }
@@ -16,7 +19,6 @@ fn main() {
     width: 30,
     height: 20,
   };
-  
-  println!("The area of the rectangle is {}.", rect1.area());
-  
+  println!("The area of the rectangle is {}.", rect1.area()); 
+
 }
