5078c62c03feea600401059103b85673c735537c
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-17 23:57:50 +0200

    Update Enum.rs

--- Code/Enum.rs
+++ Code/Enum.rs
@@ -12,4 +12,7 @@ fn main() {
   // we can create instances of the "enum" class
   let v_four = IpAddress::V4;
   let v_six = IpAddress::V6;
+  
+  route(IpAddress::V4);
+  route(v_six); // same as "route(IpAddress::V6);"
 }
