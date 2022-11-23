9dcadb7635ab54c7c1661cc3568d2f1f563bd8bb
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-23 23:33:21 +0200

    Update Creating paths.rs

--- Code/Creating paths.rs
+++ Code/Creating paths.rs
@@ -28,7 +28,17 @@ fn function2() -> IoResult<()> {}
 
 ///////////
 
+mod front_of_house {
+  pub mod hosting {
+    pub fn add_to_waitlist();
+  }
+}
 
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+  hosting::add_to_waitlist();
+}
 
 ///////////
 
