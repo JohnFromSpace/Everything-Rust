1924fd9d51389b270ab1ca5b1b245a872bd11511
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-23 23:39:15 +0200

    Update Vector.rs

--- Code/Data Structures/Vector.rs
+++ Code/Data Structures/Vector.rs
@@ -2,5 +2,10 @@ fn main() {
   let vector1: Vec<u32> = Vec::new(); 
   let vector2 = vec![1, 2, 3];
   
+  let mut vector3 = Vec::new();
+  
+  vector3.push(5);
+  vector3.push(23);
+  vector3.push(1021);
   
 }
