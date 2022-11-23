2854cb2782239e7873afbd4c52f6041311075234
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-24 00:47:55 +0200

    Update UTF-8 Strings.rs

--- Code/Data Structures/UTF-8 Strings.rs
+++ Code/Data Structures/UTF-8 Strings.rs
@@ -1,3 +1,28 @@
 fn main() {
-
+  let mut s = String::new();
+  
+  let data = "initial data";
+  let s = data.to_string();
+   
+  let s = "intial contents".to_string();
+  
+  let s = String::from("intial contents");
+  
+  // Appending a String to String
+  let s1 = String::from("Hello, ");
+  let s2 = String::from("World!");
+  s1.push_str(s2);
+  println!("{}", s1); // "Hello, World!"
+  
+  let s3 = String::from("Foo");
+  let s4 = "Bar";
+  s3.push_str(s4);
+  println!("{}", s3); // "FooBar"
+  
+  let s = String::from("My name is ");
+  s.push('L');
+  println!("{}", s);
+  
+  
+  
 }
