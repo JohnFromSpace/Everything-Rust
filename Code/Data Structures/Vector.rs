36e99d2556f9ed8235171f8866c587195134c5df
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-23 23:41:50 +0200

    Update Vector.rs

--- Code/Data Structures/Vector.rs
+++ Code/Data Structures/Vector.rs
@@ -9,7 +9,12 @@ fn main() {
   vector3.push(1021);
   
   let vector4 = vec![1, 2, 3, 4, 5];
-  let third: &i32 = &v[2];
+  let third: &i32 = &vector4[2];
   println!("The third element is {}", third);
   
+  let third: Option<i32> = vector4.get(2);
+  match third {
+    Some(third) => println!("The third element is {}", third);
+    None => println!("There is no third element");
+  }
 }
