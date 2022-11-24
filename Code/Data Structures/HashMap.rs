ab6f3627bc6b06de7743e0b528a1c0907591c001
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-24 02:07:21 +0200

    Update HashMap.rs

--- Code/Data Structures/HashMap.rs
+++ Code/Data Structures/HashMap.rs
@@ -5,6 +5,9 @@ fn main() {
   hm.insert(String::from("A"), 10); // "A" is associated/paired with 10
   hm.insert(String::from("B"), 2); // "B" is paired with 2
   
+  // access a HashMap's value it stores 
+  let name = String::from("B");
+  let hash_map = hm.get(&name).copy().unwrap_or(0); // 2
   
   
 }
