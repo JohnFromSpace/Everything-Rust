5dcf292f0e06776c010c0b8141ff1562143abb10
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-24 02:05:28 +0200

    Update HashMap.rs

--- Code/Data Structures/HashMap.rs
+++ Code/Data Structures/HashMap.rs
@@ -1,5 +1,10 @@
 use std::collections::HashMap;
 
 fn main() {
+  let mut hm = HashMap::new(); 
+  hm.insert(String::from("A"), 10); // "A" is associated/paired with 10
+  hm.insert(String::from("B"), 2); // "B" is paired with 2
+  
+  
   
 }
