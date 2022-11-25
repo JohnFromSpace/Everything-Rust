a53c14777f39f59bc0123597db96f26bf2b07545
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-25 04:28:28 +0200

    Update Wordle.rs

--- Code/Programs/Wordle.rs
+++ Code/Programs/Wordle.rs
@@ -6,5 +6,8 @@ const ALL_WORDS: &str = include_str!("words.txt");
 const WORD_LENGTH: usize = 5;
 const MAX_TRIES: usize = 6;
 
+fn fix_word(word: &str) -> String {
+  word.trim().to_uppercase().chars().filter(|c| c.is_ascii_alphabetic()).collect
+}
 
 fn main() {}
