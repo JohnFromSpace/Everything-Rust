4c5c90be2b0a0ec4bc8c02bf5fe0c20aaf5a4018
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-25 04:27:15 +0200

    Update Wordle.rs

--- Code/Programs/Wordle.rs
+++ Code/Programs/Wordle.rs
@@ -2,4 +2,9 @@ use colored::*;
 use bracket_random::prelude::RandomNumberGenerator;
 use std::collections::HashSet;
 
+const ALL_WORDS: &str = include_str!("words.txt");
+const WORD_LENGTH: usize = 5;
+const MAX_TRIES: usize = 6;
+
+
 fn main() {}
