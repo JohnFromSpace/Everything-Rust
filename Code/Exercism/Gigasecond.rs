2ca711bc5fe7f5245f94ddaf6cc26d091ba4921f
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-14 22:59:41 +0200

    Create Gigasecond.rs

---
+++ Code/Exercism/Gigasecond.rs
@@ -0,0 +1,6 @@
+use time::PrimitiveDateTime as DateTime;
+use time::Duration;
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    start + Duration::seconds(1000000000)
+}
