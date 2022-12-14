849dcfa79c2cc1700e63ddd96910355fd50d6b20
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-14 22:57:14 +0200

    Create Semi Structured Logs.rs

---
+++ Code/Exercism/Semi Structured Logs.rs
@@ -0,0 +1,24 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+/// various log levels
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    let level = format!("{:?}", level);
+    format!("[{}]: {}", level.to_uppercase(), message)
+}
+pub fn info(message: &str) -> String {
+    log(LogLevel::Info, message)
+}
+pub fn warn(message: &str) -> String {
+    log(LogLevel::Warning, message)
+}
+pub fn error(message: &str) -> String {
+    log(LogLevel::Error, message)
+}
