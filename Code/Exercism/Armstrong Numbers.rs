f6faed938b54ceb978bf1768ffac479eb02a2e7e
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-14 23:03:13 +0200

    Update Armstrong Numbers.rs

--- Code/Exercism/Armstrong Numbers.rs
+++ Code/Exercism/Armstrong Numbers.rs
@@ -1,1 +1,30 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    if num < 10 {
+        return true;
+    }
+               
+    if num < 100 {
+        return false;
+    }
 
+    let order = order(num);
+    let mut total: u64 = 0;
+    let mut temp = num;
+
+    while temp != 0 {
+        let digit = (temp % 10) as u64;
+        total += digit.pow(order);
+        temp /= 10;
+    }
+
+    return total == num as u64;
+}
+
+fn order(mut num: u32) -> u32 {
+    let mut count = 0;
+    while num != 0 {
+        count += 1;
+        num /= 10;
+    }
+    return count;
+}
