06cc1ed54f15c14e5b56f79088e993e632d779a1
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-16 18:32:52 +0200

    Update Fibbonaci Sequence.rs

--- Code/Exercism/Fibbonaci Sequence.rs
+++ Code/Exercism/Fibbonaci Sequence.rs
@@ -3,4 +3,10 @@ pub fn create_empty() -> Vec<u8> {
     vec![]
 }
 
+/// Create a buffer of `count` zeroes.
+///
+/// Applications often use buffers when serializing data to send over the network.
+pub fn create_buffer(count: usize) -> Vec<u8> {
+    create_buffer(count)
+}
 
