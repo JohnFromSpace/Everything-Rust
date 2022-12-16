476376198358d9fa10b5eacbeed7167e6fc385f6
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-17 00:47:38 +0200

    Update Space Age.rs

--- Code/Exercism/Space Age.rs
+++ Code/Exercism/Space Age.rs
@@ -28,3 +28,12 @@ pub struct Jupiter;
 pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
+
+impl Planet for Mercury { fn period() -> f64 { EARTH_PERIOD * 0.2408467 } }
+impl Planet for Venus   { fn period() -> f64 { EARTH_PERIOD * 0.61519726 } }
+impl Planet for Earth   { fn period() -> f64 { EARTH_PERIOD } }
+impl Planet for Mars    { fn period() -> f64 { EARTH_PERIOD * 1.8808158 } }
+impl Planet for Jupiter { fn period() -> f64 { EARTH_PERIOD * 11.862615 } }
+impl Planet for Saturn  { fn period() -> f64 { EARTH_PERIOD * 29.447498 } }
+impl Planet for Uranus  { fn period() -> f64 { EARTH_PERIOD * 84.016846 } }
+impl Planet for Neptune { fn period() -> f64 { EARTH_PERIOD * 164.79132 } }
