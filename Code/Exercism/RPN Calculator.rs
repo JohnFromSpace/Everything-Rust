cd5a8e75771aaf5b0d60521105af3fa39617aca6
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-16 18:50:23 +0200

    Update RPN Calculator.rs

--- Code/Exercism/RPN Calculator.rs
+++ Code/Exercism/RPN Calculator.rs
@@ -6,3 +6,36 @@ pub enum CalculatorInput {
     Divide,
     Value(i32),
 }
+
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
+    if inputs.len() == 0 {
+        return None;
+    }
+    
+    let mut stack: Vec<i32> = Vec::new();
+    
+    for input in inputs {
+        match input {
+            CalculatorInput::Value(number) => stack.push(*number),
+            _ => {
+                if stack.len() < 2 {
+                    return None;
+                }
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                match input {
+                    CalculatorInput::Add => stack.push(a + b),
+                    CalculatorInput::Multiply => stack.push(a * b),
+                    CalculatorInput::Subtract => stack.push(a - b),
+                    CalculatorInput::Divide => stack.push(a / b),
+                    _ => return None
+                }
+            }
+        }
+    }
+    
+    if stack.len() != 1 {
+        return None;
+    }
+    return stack.pop()
+}
