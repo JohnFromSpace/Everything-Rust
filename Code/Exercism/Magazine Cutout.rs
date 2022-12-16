acf337d507f33f05ed89b668dcdb88eea7874318
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-16 18:24:14 +0200

    Update Magazine Cutout.rs

--- Code/Exercism/Magazine Cutout.rs
+++ Code/Exercism/Magazine Cutout.rs
@@ -5,5 +5,10 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-        
+    let magazine_words = magazine.iter().fold(HashMap::new(), |mut words, str| {
+        *words.entry(str).or_insert(0) += 1;
+        words
+    });
+    
+    
 }
