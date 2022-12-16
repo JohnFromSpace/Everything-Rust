72f6c838e4ae85aebb25a5eb75602a001e4fea45
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-17 00:47:10 +0200

    Update Space Age.rs

--- Code/Exercism/Space Age.rs
+++ Code/Exercism/Space Age.rs
@@ -15,6 +15,7 @@ const EARTH_PERIOD: f64 = 31557600.0;
 
 pub trait Planet {
     fn years_during(d: &Duration) -> f64 {
-        
+        d.s as f64  / Self::period()    
     }
+    fn period() -> f64; 
 }
