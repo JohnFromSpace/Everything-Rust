8032d96308b4ea8a527eb83ac6517329b9d82577
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-16 20:32:46 +0200

    Update Anagram.rs

--- Code/Exercism/Anagram.rs
+++ Code/Exercism/Anagram.rs
@@ -3,10 +3,10 @@ use std::collections::HashSet;
 pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let old_word = word.to_lowercase();
     let mut new_word = old_word.chars().collect::<Vec<char>>();
+    
     new_word.sort_unstable();
-    possible_anagrams
-        .iter()
-        .fold(HashSet::new(), |mut acc, val| {
+    
+    possible_anagrams.iter().fold(HashSet::new(), |mut acc, val| {
             let old_val = val.to_lowercase();
             let mut new_val = old_val.chars().collect::<Vec<char>>();
             new_val.sort_unstable();
