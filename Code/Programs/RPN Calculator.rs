e579058681e8c62028eb0b03132e551bc2512a19
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-16 19:04:20 +0200

    Update RPN Calculator.rs

--- Code/Programs/RPN Calculator.rs
+++ Code/Programs/RPN Calculator.rs
@@ -3,6 +3,18 @@ use std::io::prelude::*;
 fn main() {
     let inputs = std::io::stdin();
     for s in inputs.lock().lines() {
-        
+        let mut stack: Vec<f64> = vec![];
+        let mut error = false;
+        for token in s.unwrap().split_whitespace() {
+            if let Ok(x) = token.parse() {
+                stack.push(x);
+            }
+            
+            else {
+                error = stack.len() < 2;
+                if error {break;}
+                let (y, x) = (stack.pop().unwrap(), stack.pop().unwrap());  
+            }
+        }
     }
 }
