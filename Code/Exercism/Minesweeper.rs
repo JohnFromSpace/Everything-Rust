ef3c87277152021e9486fa1ec0698c6fcf24eaeb
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-17 01:13:06 +0200

    Update Minesweeper.rs

--- Code/Exercism/Minesweeper.rs
+++ Code/Exercism/Minesweeper.rs
@@ -1,5 +1,10 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     if minefield.is_empty() {
         return vec![];
-    } 
+    }
+    
+     if minefield[0].is_empty() {
+        return vec![String::new()];
+    }
+    
 }
