b1a47a5e85253e075f1c103385ee76953f2a6f8b
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-16 18:22:17 +0200

    Update Magazine Cutout.rs

--- Code/Exercism/Magazine Cutout.rs
+++ Code/Exercism/Magazine Cutout.rs
@@ -1,1 +1,7 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+use std::collections::HashMap;
+
 
