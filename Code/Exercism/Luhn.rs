dd353e22363a54a670710e3ff2945f1cf8e0a9b1
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-19 00:28:53 +0200

    Update Luhn.rs

--- Code/Exercism/Luhn.rs
+++ Code/Exercism/Luhn.rs
@@ -4,4 +4,20 @@ pub fn is_valid(code: &str) -> bool {
         return false;
     }
     
+    let adjust = |(i, n)| -> u32 {
+        if i % 2 == 0 {
+            n
+        }
+        else {
+            let new_number = n * 2;
+            if new_number >= 10 {
+                new_number - 9
+            }
+            else {
+                new_number
+            }
+        }
+    };
+    
+    
 }
