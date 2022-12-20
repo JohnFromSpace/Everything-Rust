e1464a08d69a5997aedf0fb0315aa662a183c51e
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-12-21 00:46:26 +0200

    Update Macros.rs

--- Code/Exercism/Macros.rs
+++ Code/Exercism/Macros.rs
@@ -1,6 +1,8 @@
 #[macro_export]
 macro_rules! hashmap {
-    () => {
-        unimplemented!()
-    };
+    ($($( $key: expr => $val: expr )+$(,)?)*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $($( map.insert($key, $val); )*)*
+         map
+    }}
 }
