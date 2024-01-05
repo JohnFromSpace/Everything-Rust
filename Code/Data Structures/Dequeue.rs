b12fb2d68c70872b98bc14080f02702bb1cc5ccb
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-05 11:30:41 +0200

    Update Dequeue.rs

--- Code/Data Structures/Dequeue.rs
+++ Code/Data Structures/Dequeue.rs
@@ -1,1 +1,16 @@
+use std::collections::VecDeque;
 
+fn main() {
+    // Create a new empty queue
+    let mut queue = VecDeque::new();
+
+    // Enqueue elements
+    queue.push_back(1);
+    queue.push_back(2);
+    queue.push_back(3);
+
+    // Dequeue elements
+    while let Some(front) = queue.pop_front() {
+        println!("Dequeued: {}", front);
+    }
+}
