4cdc75d84facc55da0b778549d37009f29bfc75e
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-05 11:31:08 +0200

    Update Queue.rs

--- Code/Data Structures/Queue.rs
+++ Code/Data Structures/Queue.rs
@@ -1,1 +1,15 @@
+fn main() {
+    // Create a new empty queue
+    let mut queue = Vec::new();
 
+    // Enqueue elements
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+
+    // Dequeue elements
+    while let Some(front) = queue.get(0).cloned() {
+        println!("Dequeued: {}", front);
+        queue.remove(0);
+    }
+}
