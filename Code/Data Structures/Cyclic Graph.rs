4ba99af300ba20abc72387f0cab0574710a4af9a
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-06 13:19:44 +0200

    Update Cyclic Graph.rs

--- Code/Data Structures/Cyclic Graph.rs
+++ Code/Data Structures/Cyclic Graph.rs
@@ -13,4 +13,19 @@ impl Graph {
     fn add_edge(&mut self, from: usize, to: usize) {
         self.edges.entry(from).or_insert(Vec::new()).push(to);
     }
+
+    fn has_cycle(&self) -> bool {
+        let mut visited = HashSet::new();
+        let mut recursion_stack = HashSet::new();
+
+        for &node in self.edges.keys() {
+            if !visited.contains(&node) {
+                if self.detect_cycle(node, &mut visited, &mut recursion_stack) {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
 }
