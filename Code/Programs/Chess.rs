25f71398f2b6c056d5640b5489d649a9a30103ed
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-07 15:35:45 +0200

    Update Chess.rs

--- Code/Programs/Chess.rs
+++ Code/Programs/Chess.rs
@@ -52,6 +52,16 @@ impl Board {
             self.add_piece(1, i, PieceType::Pawn, Color::White);
             self.add_piece(6, i, PieceType::Pawn, Color::Black);
         }
-
         
+        self.add_piece(7, 0, PieceType::Rook, Color::Black);
+        self.add_piece(7, 1, PieceType::Knight, Color::Black);
+        self.add_piece(7, 2, PieceType::Bishop, Color::Black);
+        self.add_piece(7, 3, PieceType::Queen, Color::Black);
+        self.add_piece(7, 4, PieceType::King, Color::Black);
+        self.add_piece(7, 5, PieceType::Bishop, Color::Black);
+        self.add_piece(7, 6, PieceType::Knight, Color::Black);
+        self.add_piece(7, 7, PieceType::Rook, Color::Black);
+    }
+
+    
 }
