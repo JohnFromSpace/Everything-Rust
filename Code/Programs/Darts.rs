2ca1a7454928b95e39fff19eea09f1bfb8cb0ad4
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-09 16:02:23 +0200

    Update Darts.rs

--- Code/Programs/Darts.rs
+++ Code/Programs/Darts.rs
@@ -1,3 +1,11 @@
 use rand::Rng;
 use std::collections::{HashMap, BTreeMap};
 use std::io;
+
+#[derive(Debug, Clone)]
+struct Player {
+    name: String,
+    proficiency: f64,
+    total_score: usize,
+    average_score: f64,
+}
