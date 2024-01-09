68b56f6a8bd3b11bdd1685998df588413f122fe6
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-09 14:48:13 +0200

    Update Darts (simplified).rs

--- Code/Programs/Darts (simplified).rs
+++ Code/Programs/Darts (simplified).rs
@@ -3,3 +3,10 @@ use std::io;
 
 const DAYS_IN_A_WEEK: usize = 7;
 const WEEKS_IN_A_YEAR: usize = 52;
+
+#[derive(Debug)]
+struct Player {
+    name: String,
+    proficiency: f64,
+    cumulative_score: usize,
+}
