15373a3a1a51aa37c4de143bf6c3854ef36feef0
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-09 16:48:26 +0200

    Update Darts.rs

--- Code/Programs/Darts.rs
+++ Code/Programs/Darts.rs
@@ -88,16 +88,9 @@ impl Game {
     }
     
     fn determine_winner(&self) -> Vec<String> {
-        let mut winners = Vec::new();
         let max_score = self.players.iter().map(|p| p.total_score).max().unwrap_or(0);
 
-        for player in &self.players {
-            if player.total_score == max_score {
-                winners.push(player.name.clone());
-            }
-        }
-
-        winners    
+           
      }
 }
 
