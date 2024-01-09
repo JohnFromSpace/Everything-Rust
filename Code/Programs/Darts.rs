56e0c655185eafa58f3780aaebf7dccb081e33c4
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-09 16:50:02 +0200

    Update Darts.rs

--- Code/Programs/Darts.rs
+++ Code/Programs/Darts.rs
@@ -111,9 +111,7 @@ impl Tournament {
     fn play_tournament(&mut self) {
         for (index, game) in self.games.iter_mut().enumerate() {
                 println!("Game {} of the tournament", index + 1);
-                game.play_round();
-                game.display_scores();
-                game.calculate_average_scores();
+                game.play_round(index + 1);
         }
 
         println!("Tournament Over!");
