a1bc925c337c72953080e3bd26978b3319ae00fe
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-09 17:08:43 +0200

    Update Darts (original).rs

--- Code/Programs/Darts (original).rs
+++ Code/Programs/Darts (original).rs
@@ -129,5 +129,9 @@ struct League {
 impl League {
     fn new(players: Vec<Player>, games: Vec<Game>) -> Self {
         League { players, games }    
-    }    
+    }  
+
+    fn play_league(&mut self) {
+        
+    }
 }
