46919036774e3f5cd34ec77eda5e0ab12ef0e07a
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-11 15:33:18 +0200

    Update Kripke-Platek Set Theory.rs

--- Code/Programs/Kripke-Platek Set Theory.rs
+++ Code/Programs/Kripke-Platek Set Theory.rs
@@ -19,5 +19,10 @@ impl KripkePlaket {
             universe: HashSet::new(),
             admissible_sets: HashSet::new(),
         }     
-    }   
+    } 
+
+    // Axiom of extensionality: For all sets x and y, x = y if and only if for all z, z is in x if and only if z is in y.
+    fn axiom_of_extensionality(&self, set1: &HashSet<VonNeumann>, set2: &HashSet<VonNeumann>) -> bool {
+        set1 == set2
+    }
 }
